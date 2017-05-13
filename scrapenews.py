d9fbae65587c0f73e9b5207b3e00421409555317
minfun <nowican@live.com>
2017-05-13 22:13:19 +0800

    update

---
+++ scrapenews.py
@@ -0,0 +1,8 @@
+ios_safari_USER_AGENT = 'Mozilla/5.0 (iPhone; CPU iPhone OS 10_3_1 like Mac OS X) AppleWebKit/603.1.30 (KHTML, like Gecko) Mobile/14E304 QQ/7.0.1.407 V1_IPH_SQ_7.0.1_1_APP_A Pixel/750 Core/UIWebView NetType/WIFI QBWebViewType/1'
+ios_qqbrowser_USER_AGENT = 'Mozilla/5.0 (iPhone 91; CPU iPhone OS 10_3_1 like Mac OS X) AppleWebKit/603.1.30 (KHTML, like Gecko) Version/10.0 MQQBrowser/7.4.1 Mobile/14E304 Safari/8536.25 MttCustomUA/2 QBWebViewType/1 WKType/1'
+Q_GUID = '22e84d5ccf373bbe95ccc1ec118c88cb'
+Q_UA = 'IQB74_GA/743076&IMTT_3/743076&IPH&468340&iPhone91&50001&9973&iOS10.3.1&V3'
+Q_UA2 = 'QV=2&PL=IOS&PR=QB&DE=PHONE&VE=GA&VN=7.4.1.3076&CO=IWK&RL=750*1334&MO=iPhone91&CHID=50001&LCID=9973&OS=10.3.1&REF=com.tencent.mobileqq_0'
+Q_AUTH = 'd551ec296abc979f2c92ad2a96be10c509d5fa47edafd9ce'
+REQ = 'GET /getQQNewsComment?id=20170512A0COAB00&comment_id=1932150388&from=share&lcount=2&v=1494669235455&otype=jsonp&_=1494669235456&callback=jsonp1 HTTP/1.1'
+
